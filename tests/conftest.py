74439e8709e8d56c3a2b18dd115b5b0e91b5fa9a
William YT Hu <halomasterpizza@gmail.com>
2026-03-10 18:05:49 -0700

    Conftest so chrome runs headless

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.hookimpl
+def pytest_setup_options():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    return options
